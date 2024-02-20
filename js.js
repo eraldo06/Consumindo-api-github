87d3ce6f62addd5771320158b7013690197ef030
eraldo06 <eraldoleonardo384@gmail.com>
2024-02-19 23:56:47 -0300

    pegando os repositorios dos usuários

--- js.js
+++ js.js
@@ -9,9 +9,13 @@ const user={
     repos: Array,
 }
 
+// pegando a pesquisa do usuário
 const query = document.querySelector(".query");
 
+
+// procurando usuário na api
 async function getUser(){
+    getRepos()
     const url = `https://api.github.com/users/${query.value}`;
     const data = await fetch(url);
     const user_data = await data.json();
@@ -29,4 +33,58 @@ async function getUser(){
     user.public_repos = user_data.public_repos;
     user.followers = user_data.followers;
     user.following = user_data.following;
+
+    getAllData()
+}
+
+
+// pegando os repositórios
+async function getRepos(){
+    const url = `https://api.github.com/users/${query.value}/repos`;
+    const data = await fetch(url);
+    const repos = await data.json();
+
+    user.repos = repos;
+}
+
+
+// colocando os dados no html
+function getAllData(){
+    const user_info = document.getElementById("user-info")
+
+    user_info.setAttribute("style", "display: block")
+
+    user_info.innerHTML=`
+        
+    <div class="user-info">
+    <div class="user-avatar">
+        <h2 class="user-name">${user.name}</h2>
+        <h3 class="user-login">${user.login}</h3>
+        <img class="avatar_img" src="${user.avatar_url}" alt="${user.login}">
+        <p class="bio">${user.bio}</p>
+    </div>
+    <div class="user-status">
+        <h2>Info</h2>
+        <p>Repos: ${user.public_repos}</p>
+        <p>Followers: ${user.followers}</p>
+        <p>Following: ${user.following}</p>
+    </div>
+    </div>
+
+    ${user.repos.map((repo) =>
+        `
+        <div class="repo">
+        <h3 class="repo-name">${repo.name}</h3>
+        <p class="repo-description">${repo.description}</p>
+        <p class="repo-language">${repo.language}</p>
+        <div class="repo-stats">
+            <span>${repo.stargazers_count}</span>
+            <span>${repo.watchers_count}</span>
+            <span>${repo.forks_count}</span>
+            <span>${new Date(repo.update_at).toLocaleDateString('pt-br')}</span>
+        </div>
+        </div>
+        `
+    )}
+    `
 }
